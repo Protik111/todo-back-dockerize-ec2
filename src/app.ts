4fe3c919d4ff18a81508bcaab5ade4dd0bdba874
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-11-28 23:48:31 +0600

    [NOT DONE] todo routing

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import routes from "./app/routes";
 import cookieParser from "cookie-parser";
 
 const app: Application = express();
+const corsOptions = {
+  origin: "*",
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 //parser
